aacf69fe1f7aefb7d3ece9cda20f433b30ab16ca
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-06-07 16:36:03 +0100

    Snake in matrix

---
+++ src/main/java/leetCode/LiveSessionWeek10.java
@@ -0,0 +1,12 @@
+package leetCode;
+
+//public class LiveSessionWeek10 {int[] nums, int target
+ // 10^8 operations in 1 second
+    // int = 4 bytes
+
+//    for (int i = 0; i<nums.length; i++) {
+//        for (int j=0; i<nums.length; ++j) {
+//
+//        }
+//    }
+//}
